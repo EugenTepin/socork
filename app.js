59a457c412047110e91767aeaf7a28c68835613f
EugenTepin <4godwokr@gmail.com>
2019-07-10 16:51:12 +0400

    jwt auth

--- app.js
+++ app.js
@@ -1,35 +1,77 @@
 var express = require('express')
-var fs = require('fs')
-const port = 3000
+//var fs = require('fs')
+var jwt = require('jsonwebtoken')
+const port = 3000;
+const secret = 'do not tell anyone!';
 var cors = require('cors')
-var app = express()
+var app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 // app.use(express.static('public'))
 
+var options = {
+    root: __dirname,
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+}
 
-app.get('/users', function (req, res, next) {
-    fs.readFile('./data/users.json', 'utf8', (err, data) => {
-        if (err) {
-            res.json({ 'msg': err });
-        }
-        var obj = JSON.parse(data);
-        // делаем что-то с obj
-        res.json(obj);
-    });
 
+app.post('/login', function (req, res, next) {
+    // fs.readFile('./data/users.json', 'utf8', (err, data) => {
+    //     if (err) {
+    //         res.json({ 'msg': err });
+    //     }
+    //     var obj = JSON.parse(data);
+    //     // делаем что-то с obj
+    //     res.json(obj);
+    // });
+
+})
+
+app.post('/register', function (req, res, next) {
+    //
+    //console.log(req.body);
+    var payload = {
+        token: jwt.sign(req.body, secret)
+    }
+    res.json(payload);
 })
 
 
 
 app.get('/', function (req, res) {
-
-    fs.readFile('./index.html', 'utf8', (err, data) => {
+    res.sendFile('index.html', options, function (err) {
         if (err) {
-            res.send(err);
+            next(err)
+        } else {
+            //console.log('Sent:', fileName)
         }
-        res.send(data);
-    });
+    })
+})
+
+app.get('/signup.html', function (req, res) {
+    res.sendFile('signup.html', options, function (err) {
+        if (err) {
+            next(err)
+        } else {
+            //console.log('Sent:', fileName)
+        }
+    })
+})
+
+
+app.get('/protected.html', function (req, res) {
+    res.sendFile('protected.html', options, function (err) {
+        if (err) {
+            next(err)
+        } else {
+            //console.log('Sent:', fileName)
+        }
+    })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
